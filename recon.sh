92998c00e21aaabcae627b1e12e5c1a08cb4aa74
Zamba <ma7amd.sala7@gmail.com>
2020-09-17 14:13:27 +0400

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -19,27 +19,39 @@ then
   export name
   
   echo "Starting Just Port Scanning"
-  nmap -sS -p- $target > recon/PortsOnly
+  nmap -sS -p- $target > recon/Ports.nmap
   sleep 1
+
+  # fine tuning the ports extracted
+  cat Ports.nmap | grep "open" | grep -v "filtered" | cut -d "/" -f 1 | sort -u | sort | xargs | tr ' ' ',' > OnlyPort.txt
+
   echo "Starting Just in Details Port Scanning"
-  nmap -sC -sV -A -p- $target > recon/initial_nmap.txt
+  nmap -sC -sV -A -p $(cat OnlyPort.txt) $target > recon/initial_nmap.txt
   sleep 1
+  
   echo "Starting Just UDP 100 Ports Scanning"
   nmap -sU --top-ports 1000 $target > recon/UDPscan 
   sleep 1
+  
   #masscan -p1-65535 -oL recon/allports.txt --rate=1000 -vv -Pn $target
   echo "Checking if we have a Webserver Port avialable"
+  
   #if [ cat recon/PortsOnly | grep 80 ]; then
   echo "Creating a Wordlist from the WebSite Content"
   cewl -d 10 -m 3 --with-number http://$target > recon/SiteWordlist.txt 
+  
   echo "Brute Forcing Directories"
   /opt/git/bruteforcing/dirsearch/dirsearch.py -u http://$target -x 403,503 -e *  > recon/UDPscan
+  
   echo "Brute Forcing Files"
   ffuf -c -v -w /opt/git/password/wordlists/SecLists/Discovery/Web-Content/big.txt -u http://$target/FUZZ | tee recon/directories.txt
+  
   echo "Brute Forcing SubDomains"
   ffuf -c -v -w /opt/git/password/wordlists/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -u http://FUZZ.$target | tee subdomains.txt
+  
   echo "Scanning WebContent"
   nikto -h http://$target > recon/Nikto
+  
   #else
   #  echo "No WebServer Exist"
   #fi
